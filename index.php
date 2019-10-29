175436187f718243f295a3f408bd958408848bc6
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2019-10-29 11:18:15 -0300

    Arquivo index.php (somente testes)

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Página Hello!</title>
+</head>
+<body>
+
+    <?php        
+        echo 'Hello Leonardo Anselmo. <br>'; 
+        $var1 = "PHP";
+        $variavelValor = 50.15;
+
+        echo $var1 . "<br>";
+        echo $variavelValor . "<br>";
+        
+        define("PI", 3.14);
+
+        $resultado = 3 * PI;
+        echo $resultado . "<br>";
+    ?>
+
+</body>
+</html>
+
