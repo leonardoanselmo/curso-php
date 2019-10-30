645232a85b9efb4abbc2326c9cb240791394e51f
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2019-10-30 17:57:58 -0300

    Arquivo index.php

--- index.php
+++ index.php
@@ -44,8 +44,29 @@
         echo round(5.55, 1, PHP_ROUND_HALF_EVEN). "<BR>";
         echo round(5.55, 1, PHP_ROUND_HALF_ODD). "<BR>";
 
+        if(3 < 4 || 10 < 8) {
+            echo "Condição aceita."."<br>";
+        } else {
+            echo "Negado!"."<br>";
+        }
 
+        $x = 10 / 2 == 5 ? "É cinco" : "Não é cinco";
+        echo $x."<br>";
 
+        for($i = 0; $i < 10; $i++){
+            echo $i . " ";
+        }
+        echo "<br>";
+
+        for($i = 0; $i < 10; ++$i){
+            echo $i . " ";
+        }
+        echo "<br>";
+
+        echo $_SERVER["SERVER_ADDR"]."<br>";
+        echo $_SERVER["SERVER_NAME"]."<br>";
+        echo $_SERVER["HTTP_USER_AGENT"]."<br>";
+        echo $_SERVER["REMOTE_ADDR"]."<br>";
     ?>
 
 </body>
