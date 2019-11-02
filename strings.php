493cf1644b109b59df390b2fea43ae9fa9aaf5dc
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2019-11-02 12:00:10 -0300

    Manipulação de strings

--- strings.php
+++ strings.php
@@ -37,11 +37,59 @@
         $posicao = strpos($frase, "a", $posicao+1);
     }
 
+    // Mostra em tela do primeiro espaço até o final da string.
     $buscaString = strchr($frase, " ");
     echo $buscaString . "<br/>";
 
+    // Busca a ocorrência "para" e mostra até o final da string.
+    $buscaString = strchr($frase, "para");
+    echo $buscaString . "<br/>";
+
+    // Busca a informação do primeiro espaço em branco invertido na string.
+    $buscaString = strrchr($frase, " ");
+    echo $buscaString . "<br/>";    
+
+    // Busca numericamente uma informação na string.
+    $buscaString = substr($frase, 4);
+    echo $buscaString . "<br/>";
+
+    // Busca numericamente uma informação na string até um limite.
+    $buscaString = substr($frase, 6, 9);
+    echo $buscaString . "<br/>";
+
+    // Substitui uma informação na string por outra.
+    $buscaString = str_replace("utilizada", "criada", $frase);
+    echo $buscaString . "<br/>";
     
-    
+    // Transforma a string em caixa baixa.
+    $buscaString = strtolower($frase);
+    echo $buscaString . "<br/>";
+
+    // Transforma a string em caixa alta.
+    $buscaString = strtoupper($frase);
+    echo $buscaString . "<br/>";
+
+    // Primeira letra na frase em maiusculas.
+    $buscaString = ucfirst($frase);
+    echo $buscaString . "<br/>";
+
+    // Cada palavra da string em maiusculas.
+    $buscaString = ucwords($frase);
+    echo $buscaString . "<br/>";
+
+    // Inverte a string.
+    $buscaString = strrev($frase);
+    echo $buscaString . "<br/>";
+
+    // Quebra a string em partes separadas por espaço.
+    $buscaString = explode(" ", $frase);
+    echo $buscaString[0] . "<br/>";
+    echo $buscaString[1] . "<br/>";
+    echo $buscaString[2] . "<br/>";
+    echo $buscaString[3] . "<br/>";
+    echo $buscaString[4] . "<br/>";
+    echo $buscaString[5] . "<br/>";
+
 
     ?>    
 </body>
