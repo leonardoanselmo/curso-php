75bcf4c68a936276a101bc03d968358e6164d014
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2019-11-07 16:26:53 -0300

    Exemplos de arrays

---
+++ arrays.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Exemplos de Arrays</title>
+</head>
+<body>
+    <?php
+
+        // Cria um array e imprime o seu conteúdo.
+        $arrayExemplo = array("PHP", "SQL", 100, "Java");
+        print_r($arrayExemplo);
+
+
+    ?>
+</body>
+</html>
