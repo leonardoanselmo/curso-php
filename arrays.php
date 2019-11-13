df9d958e392b51e8948797f8bf182c2e5a704626
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2019-11-13 12:08:19 -0300

    Entendendo arrays

--- arrays.php
+++ arrays.php
@@ -26,6 +26,49 @@
         // Conta quantos elementos tem no array.
         echo "Têm quantos elementos o array? ".count($arrayExemplo)."<br/>";
 
+        // Outra forma de contar elementos no array.
+        echo "Têm quantos elementos o array? ".sizeof($arrayExemplo)."<br/>";
+
+        // Percorrendo o array
+        foreach($arrayExemplo as $elemento){
+            echo "Tem no array: ".$elemento." ,";
+        }
+        echo "<br/>";
+
+        // Adiciona um novo elemento no array.
+        array_push($arrayExemplo, "Leonardo");
+        print_r($arrayExemplo);
+        echo "<br/>";
+
+        // Retorna o último elemento do array.
+        $ultimo_elemento = array_pop($arrayExemplo);
+        echo "Retorna o último elemento: ".$ultimo_elemento."<br/>";
+
+        // Retorna o primeiro elemento do array excluindo do mesmo.
+        $primeiro_elemento = array_shift($arrayExemplo);
+        echo "Retorna o primeiro elemento excluindo do array: ".$primeiro_elemento."<br/>";
+
+        echo "Array sem o primeiro elemento: ";
+        print_r($arrayExemplo);
+        echo "<br/>";
+
+        // Adiciona um elemento na primeira posição do array.
+        echo "Adicionando um elemento na primeira posição: ";
+        array_unshift($arrayExemplo, "Anselmo");
+        print_r($arrayExemplo);
+        echo "<br/>";
+
+        
+
+
+        
+
+
+
+
+
+
+
 
         
 
