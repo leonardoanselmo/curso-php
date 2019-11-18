1760ccb7b5523d28c449d4cd24858a78fb8f5849
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2019-11-18 11:46:20 -0300

    Exemplos com arrays

--- arrays.php
+++ arrays.php
@@ -58,21 +58,88 @@
         print_r($arrayExemplo);
         echo "<br/>";
 
-        
+        // Criando um array somente com valores
+        $arrayExemplo = array(140.1, 200, 215.05, 550);
+        print_r($arrayExemplo);
+        echo "<br/>";
 
+        // Criando uma função que recebe um valor e o retorna.
+        function insereMoeda($valor){
+            $valor = "R$ ".$valor;
+            return $valor;
+        }
 
-        
+        // Acrescenta no arrayexemplo o retorno da função insereMoeda.
+        $arrayExemplo = array_map("insereMoeda", $arrayExemplo);
+        print_r($arrayExemplo);
+        echo "<br/>";
 
+        // Criando um array com novos valores.
+        $arrayExemplo = array("Linguagem1" => "PHP",
+                              "Linguagem2" => "SQL",
+                              "Linguagem3" => 100,
+                              "Linguagem4" => "Java");
+        print_r($arrayExemplo);
+        echo "<br/>";
 
+        // Verificando se existe a chave no array.
+        if(array_key_exists("Linguagem2", $arrayExemplo)){
+            echo "Existe Linguagem2 no array"."<br/>";
+        } else {
+            echo "Não existe Linguagem2 no array"."<br/>";
+        }
 
+        // Mostrando as chaves que existem no array.
+        $keys = array_keys($arrayExemplo);
+        foreach($keys as $key){
+            echo $key.", ";
+        }
+        echo "<br/>";
 
+        // Procura um elemento no array.
+        $key = array_search("SQL", $arrayExemplo);
+        echo "Chave do elemento SQL é: ".$key."<br/>";
 
+        $isIn = in_array("PHP1", $arrayExemplo);
+        if($isIn){
+            echo "Elemento existe no array."."<br/>";
+        } else {
+            echo "Elemento não existe no array."."<br/>";
+        }
 
+        print_r($arrayExemplo);
+        echo "<br/>";
+        // Embaralha o array.
+        shuffle($arrayExemplo);        
+        print_r($arrayExemplo);
+        echo "<br/>";
 
+        // Ordena o array.
+        sort($arrayExemplo);
+        print_r($arrayExemplo);
+        echo "<br/>";
 
-        
+        // Ordena o array inverso.
+        rsort($arrayExemplo);
+        print_r($arrayExemplo);
+        echo "<br/>";
 
+        // Transforma a string em um array.
+        $stringExemplo = "10;20;30;40;50";
+        $arrayExemplo = explode(";", $stringExemplo);
+        print_r($arrayExemplo);
+        echo "<br/>";
 
+        // Transforma um array em string.
+        $arrayExemplo = array("a", "b", "c", "d", "e");
+        $stringExemplo = implode(";", $arrayExemplo);
+        echo $stringExemplo;
+        echo "<br/>";
+
+        // Criando um array atraves de uma string.
+        $stringExemplo = "chave=valor&chave2=valor2&var=PHP";
+        parse_str($stringExemplo, $arrayExemplo);
+        print_r($arrayExemplo);
 
     ?>
 </body>
